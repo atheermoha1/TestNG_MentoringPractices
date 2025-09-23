158962e60bbc7a594e4f413c9f668e10f10a604d
Atheer <atheermoha17@gmail.com>
2025-09-23 20:52:20 +0300

    TestNG_Assignment

--- src/test/java/com/SdaMentoring/Assignments/day02/T01_AmazonSearchTestwithParameters.java
+++ src/test/java/com/SdaMentoring/Assignments/day02/T01_AmazonSearchTestwithParameters.java
@@ -3,19 +3,16 @@ package com.SdaMentoring.Assignments.day02;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import utilities.TestBase;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.time.Duration;
 
-public class C01_AmazonSearchTestwithParameters  {
+public class T01_AmazonSearchTestwithParameters {
 /*
 1. Navigate to: https://www.amazon.com
 2. Search for different keywords: Java, Selenium
@@ -44,6 +41,7 @@ WebDriver driver;
         //        Robot robot= new Robot();
         //        robot.keyPress(KeyEvent.VK_ENTER);
         //        robot.keyRelease(KeyEvent.VK_ENTER);
+
 
        // Assert that result text contains the searched word
         Assert.assertTrue(driver.findElement(By.xpath("//span[@class='a-color-state a-text-bold']")).getText().replace("\"","").trim().equalsIgnoreCase(keyword),"The result does not contains the keyword");
